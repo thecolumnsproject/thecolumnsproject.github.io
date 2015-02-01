8467d0d43c5fcacbb1e9daf0490aa1a59f035704
Jeremy Lubin <lubin.jeremy@gmail.com>
2015-02-01 15:23:38 -0800

    begin style spec

---
+++ specs/controllers/style-view-spec.js
@@ -0,0 +1,26 @@
+describe('Style View Spec', function() {
+
+	describe('Initialization', function() {
+
+	});
+
+	describe('Rendering', function() {
+
+	});
+
+	describe('Updating', function() {
+
+	});
+
+	describe('Listening to Template Events', function() {
+
+	});
+
+	describe('Listening to Items Events', function() {
+
+	});
+
+	describe('Updating Item Styles', function() {
+
+	});
+});

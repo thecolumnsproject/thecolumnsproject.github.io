98410aba0012da089b4c478312ac9fd57fb2bfe8
Jeremy Lubin <lubin.jeremy@gmail.com>
2015-02-06 18:25:53 -0800

    add spec sketeletons for style button groups

---
+++ specs/controllers/style-multiple-segmented-button-view-spec.js
@@ -0,0 +1,51 @@
+describe('Style Multiple Segmented Button View', function() {
+
+	describe('Initiation', function() {
+
+		it('should have the correct defaults', function() {
+
+		});
+
+		it('should allow initiation with a label', function() {
+
+		});
+
+		it('should allow initiation with a set of buttons, each with icon, property and active and inactive values', function() {
+
+		});
+	});
+
+	describe('Rendering', function() {
+
+		it('should have the right class', function() {
+
+		});
+
+		it('should have the right label', function() {
+
+		});
+
+		it('should have the right number of buttons', function() {
+
+		});
+
+		it('should render the buttons with the correct attributes', function() {
+
+		});
+	});
+
+	describe('Button Selection', function() {
+
+		it('should select the clicked button', function() {
+
+		});
+
+		it('should update its values', function() {
+
+		});
+
+		it('should notify the app of the new value', function() {
+
+		});
+	});
+});
---
+++ specs/controllers/style-segmented-button-view-spec.js
@@ -0,0 +1,63 @@
+describe('Style Segmented Button View', function() {
+
+	describe('Initiation', function() {
+
+		it('should have the correct defaults', function() {
+
+		});
+
+		it('should allow initiation with a label', function() {
+
+		});
+
+		it('should allow initiation with a property', function() {
+
+		});
+
+		it('should allow intiation with a set of buttons, each with icon and value', function() {
+
+		});
+	});
+
+	describe('Rendering', function() {
+
+		it('should have the right class', function() {
+
+		});
+
+		it('should have the right label', function() {
+
+		});
+
+		it('should have the right property', function() {
+
+		});
+
+		it('should have the right number of buttons', function() {
+
+		});
+
+		it('should render the buttons with the correct values', function() {
+
+		});
+	});
+
+	describe('Button Selection', function() {
+
+		it('should select the clicked button', function() {
+
+		});
+
+		it('should deselect the other buttons', function() {
+
+		});
+
+		it('should update its value', function() {
+
+		});
+
+		it('should notify the app of the new value', function() {
+
+		});
+	});
+});

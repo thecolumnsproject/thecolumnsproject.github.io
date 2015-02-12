8312acbd3a4a650d0c99e6fe120179da868ad0cc
Jeremy Lubin <lubin.jeremy@gmail.com>
2015-02-11 22:54:15 -0800

    add upload view spec skeleton

---
+++ specs/controllers/upload-view-spec.js
@@ -0,0 +1,34 @@
+describe('Upload View', function() {
+
+	describe('Initialization', function() {
+
+	});
+
+	describe('Rendering', function() {
+
+	});
+
+	describe('Choosing a File', function() {
+
+	});
+
+	describe('Parsing a CSV', function() {
+
+	});
+
+	describe('Uploading Table Data', function() {
+
+	});
+
+	describe('Hiding the Upload View', function() {
+
+	});
+
+	describe('Showing the Upload View', function() {
+
+	});
+
+	describe('Updating the Loading Message', function() {
+
+	});
+});

3c4f02226b9d57931debbb883b2a6060963dbf9a
Jeremy Lubin <lubin.jeremy@gmail.com>
2015-11-21 17:23:36 -0800

    keep column icons top-aligned when there are multiple lines of text in a column title

--- templates/templates.js
+++ templates/templates.js
@@ -164,11 +164,11 @@ function program3(depth0,data) {
   buffer += " ";
   stack1 = helpers['if'].call(depth0, (depth0 && depth0.selected), {hash:{},inverse:self.noop,fn:self.program(3, program3, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
-  buffer += "\">\n	<i class=\"icon-text layout-column-icon\"></i>\n	";
+  buffer += "\">\n	<i class=\"icon-text layout-column-icon\"></i>\n	<span class=\"layout-columns-text\">";
   if (helper = helpers.title) { stack1 = helper.call(depth0, {hash:{},data:data}); }
   else { helper = (depth0 && depth0.title); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
   buffer += escapeExpression(stack1)
-    + "\n</div>";
+    + "</span>\n</div>";
   return buffer;
   });
 
